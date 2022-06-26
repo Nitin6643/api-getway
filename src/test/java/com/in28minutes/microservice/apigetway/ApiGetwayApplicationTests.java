f5ec095d482c92753f88eaeeac77c07e5f86541c
Nitin6643 <68808626+Nitin6643@users.noreply.github.com>
2022-06-26 23:39:44 +0530

    initial commit!

---
+++ src/test/java/com/in28minutes/microservice/apigetway/ApiGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservice.apigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
